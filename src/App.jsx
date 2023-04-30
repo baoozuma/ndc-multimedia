760c2b0d76487beb3f920431e0de7779fe5cb357
unknown <ozuma1912@gmail.com>
2023-05-01 02:59:56 +0700

    added slide

--- src/App.jsx
+++ src/App.jsx
@@ -224,16 +224,16 @@ const settings = {
   infinite: false,
   speed: 1000,
   slidesToShow: 5,
-  slidesToScroll: 1,
-  swipeToScroll: true,
+  swipeToSlide: true,
   initialSlide: 0,
   responsive: [
     {
       breakpoint: 1024,
       settings: {
         slidesToShow: 3,
-        slidesToScroll: 1,
-        dots: true
+
+        dots: true,
+        swipeToSlide: true,
       }
     },  
   ]
